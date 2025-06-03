ff450a5b9bf10e2a3e0126fdfd8f407f776b3ee6
Alucard8130 <smart8130@hotmail.com>
2025-06-03 16:50:24 -0600

    permisos

--- empresas/views.py
+++ empresas/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Empresa
 from .forms import EmpresaForm
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Create your views here.
-@login_required
+#@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def empresa_crear(request):
     if request.method == 'POST':
         form = EmpresaForm(request.POST)
@@ -17,10 +19,32 @@ def empresa_crear(request):
         form = EmpresaForm()
     return render(request, 'empresas/crear.html', {'form': form})
 
+#@login_required
+#def empresa_lista(request):
+ #   empresas = Empresa.objects.all()
+  #  return render(request, 'empresas/lista.html', {'empresas': empresas})
+
+#@login_required
+#def empresa_lista(request):
+ #   if request.user.is_superuser:
+  #      empresas = Empresa.objects.all()
+   # else:
+    #    try:
+     #       perfil = request.user.perfilusuario
+      #      empresas = Empresa.objects.filter(pk=perfil.empresa.pk)
+       # except:
+        #    empresas = Empresa.objects.none()
+   # return render(request, 'empresas/lista.html', {'empresas': empresas})
+
 @login_required
 def empresa_lista(request):
-    empresas = Empresa.objects.all()
+    if request.user.is_superuser:
+        empresas = Empresa.objects.all()
+    else:
+        empresa = getattr(request.user.perfilusuario, 'empresa', None)
+        empresas = Empresa.objects.filter(pk=empresa.pk) if empresa else Empresa.objects.none()
     return render(request, 'empresas/lista.html', {'empresas': empresas})
+
 
 @login_required
 def empresa_editar(request, pk):
@@ -34,7 +58,8 @@ def empresa_editar(request, pk):
         form = EmpresaForm(instance=empresa)
     return render(request, 'empresas/editar.html', {'form': form, 'empresa': empresa})
 
-@login_required
+#@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def empresa_eliminar(request, pk):
     empresa = Empresa.objects.get(pk=pk)
     if request.method == 'POST':
--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,12 @@
+
+from django.contrib.auth.models import User
 from django.db import models
+from empresas.models import Empresa
 
 # Create your models here.
+class PerfilUsuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+       return f"{self.usuario.username} → {self.empresa.nombre if self.empresa else 'Sin empresa'}"
