68d9b72df46d1a5de7a6f16cf9d2c571d91714e8
Alucard8130 <smart8130@hotmail.com>
2025-06-05 21:48:29 -0600

    updateSetting

--- core/settings.py
+++ core/settings.py
@@ -120,13 +120,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-#LANGUAGE_CODE = 'en-us'
-
-#TIME_ZONE = 'UTC'
-
-#USE_I18N = True
-
-#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
