3581361f4529f11e7aa738e6f63bf8bbb2ab8619
Alucard8130 <smart8130@hotmail.com>
2025-06-12 21:08:38 -0600

    deploy

--- presupuestos/templatetags/math_filters.py
+++ presupuestos/templatetags/math_filters.py
@@ -8,3 +8,10 @@ def div(value, arg):
         return float(value) / float(arg)
     except (ValueError, ZeroDivisionError):
         return ''
+    
+@register.filter
+def mul(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return ''
