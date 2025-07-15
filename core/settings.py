69b053ed724b2050c1d0b3022b7c2f513ff7280f
Alucard8130 <smart8130@hotmail.com>
2025-07-15 13:23:42 -0600

    UPDATESETTINGS

--- core/settings.py
+++ core/settings.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,10 +89,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
