fb92cbac7336ae0715c069761c778e76fc729f5c
Alucard8130 <smart8130@hotmail.com>
2025-07-15 15:21:14 -0600

    update

--- core/settings.py
+++ core/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+#DEBUG = False
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'adminsoftheron.onrender.com').split(',')
 ALLOWED_HOSTS = ['adminsoftheron.onrender.com']
 
