46cf5973e047146b1beaaae5d06c2e7a46b49944
Alucard8130 <smart8130@hotmail.com>
2025-07-15 22:59:17 -0600

    update

---
+++ gastos/migrations/002_default_grupos_subgrupos.py
@@ -0,0 +1,32 @@
+from django.db import migrations
+
+def cargar_grupos_subgrupos(apps, schema_editor):
+    GrupoGasto = apps.get_model('gastos', 'GrupoGasto')
+    SubgrupoGasto = apps.get_model('gastos', 'SubgrupoGasto')
+
+    grupos = {
+        'Gastos Administracion': ['Papelería', 'Suministros', 'Honorarios', 'Servicios profesionales', 'Oficina','Asambleas', 'Comite', 'Viajes', 'Software', 'Hardware'],
+        'Gastos Extraordinarios': ['Reparaciones mayores', 'Proyectos especiales', 'Imprevistos', 'Legales','Contingencias'],
+        'Gastos Financieros': ['Comisiones bancarias', 'Intereses', 'Cobranza', 'Inversión'],
+        'Gastos Mantenimientos': [ 'Preventivo', 
+            'Correctivo', 'Equipos', 'Instalaciones', 'Software', 'Edificio','Infraestructura','Hardware'],
+        'Gastos Nomina': ['Sueldos', 'Prestaciones', 'Seguridad social', 'Impuestos', 'Bonificaciones', 'Vacaciones', 'Aguinaldo', 'Indemnizaciones','Infonavit'],
+        'Gastos Operacion': ['Transporte', 'Logística', 'Alquiler de equipos','Seguros','Proteccion Civil'],
+        'Gastos Publicidad': ['Digital', 'Impresos', 'Eventos', 'Promociones', 'Marketing','Decoracion'],
+        'Gastos Servicios': ['Agua', 'Luz', 'Teléfonia', 'Internet', 'Seguridad','Limpieza','Jardinería'],
+    }
+
+    for grupo_nombre, subgrupos in grupos.items():
+        grupo, _ = GrupoGasto.objects.get_or_create(nombre=grupo_nombre)
+        for subgrupo_nombre in subgrupos:
+            SubgrupoGasto.objects.get_or_create(grupo=grupo, nombre=subgrupo_nombre)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gastos', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(cargar_grupos_subgrupos),
+    ]
