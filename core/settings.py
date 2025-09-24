bc3797b2026a88837136e12d1eee397f44450e02
Alucard8130 <smart8130@hotmail.com>
2025-09-24 12:23:51 -0600

    mejoras

--- core/settings.py
+++ core/settings.py
@@ -183,24 +183,24 @@ else:
 
 
 
-# AWS_ACCESS_KEY_ID = os.getenv('DO_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.getenv('DO_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.getenv('DO_SPACE_NAME')
-# AWS_S3_REGION_NAME = os.getenv('DO_REGION') 
-# AWS_S3_ENDPOINT_URL = os.getenv('DO_ENDPOINT_URL')
-# AWS_S3_ADDRESSING_STYLE = "virtual"
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = 'public-read'
-# AWS_QUERYSTRING_AUTH = False
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com'
+AWS_ACCESS_KEY_ID = os.getenv('DO_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('DO_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('DO_SPACE_NAME')
+AWS_S3_REGION_NAME = os.getenv('DO_REGION') 
+AWS_S3_ENDPOINT_URL = os.getenv('DO_ENDPOINT_URL')
+AWS_S3_ADDRESSING_STYLE = "virtual"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com'
 
 #STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/'
 #STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
-# MEDIA_URL = f'https://gesacbucket.nyc3.digitaloceanspaces.com/media/'
+MEDIA_URL = f'https://gesacbucket.nyc3.digitaloceanspaces.com/media/'
 #MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/media/'
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 SENTRY_DSN = os.getenv("SENTRY_DSN_KEY")
 
