16ee7d99477458a06d8841de7af49ed863cc2f50
Alucard8130 <smart8130@hotmail.com>
2026-07-01 12:59:50 -0600

    setupSendGridRailway

--- core/settings.py
+++ core/settings.py
@@ -134,8 +134,15 @@ DATABASES = {
 
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  #testing email backend  
+
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"  # default: SMTP (Render/local)
+)
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
@@ -143,6 +150,7 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 
 # Password validation
