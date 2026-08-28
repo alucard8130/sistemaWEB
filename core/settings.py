4b349040aa5e6d4bafe7b26fb8e0347791ed9371
Alucard8130 <smart8130@hotmail.com>
2026-08-28 16:10:48 -0600

    fix>settings

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 
@@ -15,15 +14,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-#SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-#DEBUG = True
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS","gesac.up.railway.app,adminsoftheron.onrender.com,www.gesac.com.mx").split(",")
-#ALLOWED_HOSTS = ["192.168.0.159", '*']  # For development purposes, change this in production
+# DEBUG = True
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS", "gesac.up.railway.app,adminsoftheron.onrender.com,www.gesac.com.mx"
+).split(",")
+# ALLOWED_HOSTS = ["192.168.0.159", '*']  # For development purposes, change this in production
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
-    "https://gesac.up.railway.app,https://adminsoftheron.onrender.com,https://gesac.com.mx,https://www.gesac.com.mx"
+    "https://gesac.up.railway.app,https://adminsoftheron.onrender.com,https://gesac.com.mx,https://www.gesac.com.mx",
 ).split(",")
 
 # Application definition
@@ -47,7 +48,7 @@ INSTALLED_APPS = [
     "gastos",
     "presupuestos",
     "informes_financieros",
-    "storages", 
+    "storages",
     "caja_chica",
     "widget_tweaks",
     "rest_framework",
@@ -92,8 +93,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                'empresas.context_processors.empresa_actual',  #nuevo context processor para exponer la empresa actual en todos los templates
-                'acceso_empresas.context_processors.alertas_portal_context',  #nuevo context processor para exponer las alertas del portal en todos los templates
+                "empresas.context_processors.empresa_actual",  # nuevo context processor para exponer la empresa actual en todos los templates
+                "acceso_empresas.context_processors.alertas_portal_context",  # nuevo context processor para exponer las alertas del portal en todos los templates
             ],
         },
     },
@@ -111,7 +112,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 ####################BASE DE DATOS########################
 
-#desarrollo sqlite
+# desarrollo sqlite
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -119,7 +120,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 #     }
 # }
 
-#desarrollo postgres
+# desarrollo postgres
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -131,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 #     }
 # }
 
-#produccion
+# produccion
 DATABASES = {
     "default": dj_database_url.config(
         default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
@@ -139,15 +140,14 @@ DATABASES = {
 }
 
 
-
 #######################EMAIL CONFIGURATION########################
 
 EMAIL_BACKEND = os.getenv(
     "EMAIL_BACKEND",
-    "django.core.mail.backends.smtp.EmailBackend"  # default: SMTP (Render/local)
+    "django.core.mail.backends.smtp.EmailBackend",  # default: SMTP (Render/local)
 )
 
-ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
@@ -184,50 +184,69 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/login/"
 
 
-
+# if os.getenv("DEBUG", "True") == "False":
+#     STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY_TEST")
+#     STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY_TEST")
+#     STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET_TEST")
+#     ## STRIPE_PORTAL_PRICES ADMINISTRADORES Y/O COMITE
+#     STRIPE_PORTAL_WEBHOOK_SECRET = os.getenv("STRIPE_PORTAL_WEBHOOK_SECRET_TEST")
+#     STRIPE_PORTAL_PRICES = {
+#         'basico': os.getenv("STRIPE_PORTAL_PRICE_BASICO_TEST"),
+#         'profesional': os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL_TEST"),
+#         'enterprise': os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE_TEST"),
+#     }
+# else:
+#     STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+#     STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
+#     STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET")
+#     ## STRIPE_PORTAL_PRICES ADMINISTRADORES Y/O COMITE
+#     STRIPE_PORTAL_WEBHOOK_SECRET = os.getenv("STRIPE_PORTAL_WEBHOOK_SECRET")
+#     STRIPE_PORTAL_PRICES = {
+#         'basico': os.getenv("STRIPE_PORTAL_PRICE_BASICO"),
+#         'profesional': os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL"),
+#         'enterprise': os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE"),
+#     }
 if os.getenv("DEBUG", "True") == "False":
-    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY_TEST")
-    STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY_TEST")
-    STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET_TEST")
-    ## STRIPE_PORTAL_PRICES ADMINISTRADORES Y/O COMITE
-    STRIPE_PORTAL_WEBHOOK_SECRET = os.getenv("STRIPE_PORTAL_WEBHOOK_SECRET_TEST")
-    STRIPE_PORTAL_PRICES = {
-        'basico': os.getenv("STRIPE_PORTAL_PRICE_BASICO_TEST"),
-        'profesional': os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL_TEST"),
-        'enterprise': os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE_TEST"),
-    }
-else:
+    # Producción -- usa las llaves REALES
     STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
     STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
     STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET")
-    ## STRIPE_PORTAL_PRICES ADMINISTRADORES Y/O COMITE 
     STRIPE_PORTAL_WEBHOOK_SECRET = os.getenv("STRIPE_PORTAL_WEBHOOK_SECRET")
     STRIPE_PORTAL_PRICES = {
-        'basico': os.getenv("STRIPE_PORTAL_PRICE_BASICO"),
-        'profesional': os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL"),
-        'enterprise': os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE"),
+        "basico": os.getenv("STRIPE_PORTAL_PRICE_BASICO"),
+        "profesional": os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL"),
+        "enterprise": os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE"),
+    }
+else:
+    # Desarrollo -- usa las llaves de PRUEBA
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY_TEST")
+    STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY_TEST")
+    STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET_TEST")
+    STRIPE_PORTAL_WEBHOOK_SECRET = os.getenv("STRIPE_PORTAL_WEBHOOK_SECRET_TEST")
+    STRIPE_PORTAL_PRICES = {
+        "basico": os.getenv("STRIPE_PORTAL_PRICE_BASICO_TEST"),
+        "profesional": os.getenv("STRIPE_PORTAL_PRICE_PROFESIONAL_TEST"),
+        "enterprise": os.getenv("STRIPE_PORTAL_PRICE_ENTERPRISE_TEST"),
     }
 
 
-
-
-AWS_ACCESS_KEY_ID = os.getenv('DO_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('DO_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('DO_SPACE_NAME')
-AWS_S3_REGION_NAME = os.getenv('DO_REGION') 
-AWS_S3_ENDPOINT_URL = os.getenv('DO_ENDPOINT_URL')
+AWS_ACCESS_KEY_ID = os.getenv("DO_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("DO_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("DO_SPACE_NAME")
+AWS_S3_REGION_NAME = os.getenv("DO_REGION")
+AWS_S3_ENDPOINT_URL = os.getenv("DO_ENDPOINT_URL")
 AWS_S3_ADDRESSING_STYLE = "virtual"
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = "public-read"
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com'
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com"
 
 
 SENTRY_DSN = os.getenv("SENTRY_DSN_KEY")
@@ -236,14 +255,16 @@ sentry_sdk.init(
     dsn=SENTRY_DSN,
     integrations=[DjangoIntegration()],
     traces_sample_rate=1.0,  # Puedes bajarlo a 0.1 si no quieres rastreo de performance
-    send_default_pii=True,   # Para capturar datos de usuario autenticado
+    send_default_pii=True,  # Para capturar datos de usuario autenticado
 )
 
 FACTURAMA_USER = os.getenv("FACTURAMA_USER")
 FACTURAMA_PASSWORD = os.getenv("FACTURAMA_PASSWORD")
 
 
-PORTAL_PAGOS_URL = os.getenv("PORTAL_PAGOS_URL", "https://www.gesac.com.mx/visitante/login/")
+PORTAL_PAGOS_URL = os.getenv(
+    "PORTAL_PAGOS_URL", "https://www.gesac.com.mx/visitante/login/"
+)
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
